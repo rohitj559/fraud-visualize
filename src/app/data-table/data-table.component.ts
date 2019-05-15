0ffca36f478dc49f86fb17946908d474dec8b283
Rohit Jagannath <rohit@thebulletin.io>
2019-05-15 07:59:19 -0500

    Update data-table.component.ts

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../data.service';
 import { IData } from '../data';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-data-table',
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.css']
 })
-export class DataTableComponent implements OnInit {
+export class DataTableComponent implements OnInit, OnDestroy {
   rippleData: any[];
   errorMessage: string;
+  private dataSubscribe: Subscription;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.dataService.getData().subscribe(
+    this.dataSubscribe = this.dataService.getData().subscribe(
       responseData => {
         this.rippleData = responseData.slice(0, 5);
-        console.log(this.rippleData);
+        // console.log(this.rippleData);
       },
       error => this.errorMessage = error as any
     );
+  }
+
+  ngOnDestroy() {
+    this.dataSubscribe.unsubscribe();
   }
 }
