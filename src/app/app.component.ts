080d209ec339533963c1175f01007e885fcbbde7
Rohit Jagannath <rohit@thebulletin.io>
2019-05-15 07:49:34 -0500

    update app.module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Fraud Loss Visualization';
+  title = 'Fraud Loss Visualization and Prediction System';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +13,12 @@ import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.componen
 import { RadarChartComponent } from './radar-chart/radar-chart.component';
 import { PieChartComponent } from './pie-chart/pie-chart.component';
 import { LineChartComponent } from './line-chart/line-chart.component';
+import { VisualizationComponent } from './visualization/visualization.component';
+import { PredictionComponent } from './prediction/prediction.component';
+import { SideButtonsComponent } from './side-buttons/side-buttons.component';
+import { OtherChartsComponent } from './other-charts/other-charts.component';
+import { DataTableComponent } from './data-table/data-table.component';
+
 
 @NgModule({
   declarations: [
@@ -19,13 +27,20 @@ import { LineChartComponent } from './line-chart/line-chart.component';
     DoughnutChartComponent,
     RadarChartComponent,
     PieChartComponent,
-    LineChartComponent
+    LineChartComponent,
+    VisualizationComponent,
+    PredictionComponent,
+    SideButtonsComponent,
+    OtherChartsComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    ChartsModule
+    ChartsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
