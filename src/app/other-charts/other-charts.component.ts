cba4aed23c83c84aa55c9c19c26f5b28f573d182
Rohit Jagannath <rohit@thebulletin.io>
2019-05-15 07:51:28 -0500

    update other-charts.component

---
+++ src/app/other-charts/other-charts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-other-charts',
+  templateUrl: './other-charts.component.html',
+  styleUrls: ['./other-charts.component.css']
+})
+export class OtherChartsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
