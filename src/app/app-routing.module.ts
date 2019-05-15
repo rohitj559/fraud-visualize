6c544e3937c4b0fde63988c63053ed4a9318817c
Rohit Jagannath <rohit@thebulletin.io>
2019-05-15 07:49:11 -0500

    adding components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,19 @@ import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.componen
 import { RadarChartComponent } from './radar-chart/radar-chart.component';
 import { PieChartComponent } from './pie-chart/pie-chart.component';
 import { LineChartComponent } from './line-chart/line-chart.component';
+import { VisualizationComponent } from './visualization/visualization.component';
+import { PredictionComponent } from './prediction/prediction.component';
+import { OtherChartsComponent } from './other-charts/other-charts.component';
 
 const routes: Routes = [
+  {path: 'visualization', component: BarChartComponent},
+  {path: 'prediction', component: PredictionComponent},
   {path: 'bar-chart', component: BarChartComponent},
   {path: 'line-chart', component: LineChartComponent},
   {path: 'doughnut-chart', component: DoughnutChartComponent},
   {path: 'radar-chart', component: RadarChartComponent},
   {path: 'pie-chart', component: PieChartComponent},
+  {path: 'other-charts', component: OtherChartsComponent},
   {path: '**', component: BarChartComponent},
 ];
 
